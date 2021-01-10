a13c0cea47da592f8f0f9a6b37a0aef6ec7c8c56
khloud-name <khloud.mostfa00@eng-st.cu.edu.eg>
2021-01-11 01:55:31 +0200

    project

--- project.py
+++ project.py
@@ -1,9 +1,19 @@
-from flask import Flask, request, render_template
+from flask import Flask,jsonify,request,render_template
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="mysql",
+    database="hosmansys"
+)
+mycursor = mydb.cursor()
+
 app = Flask(__name__)
 
-
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 
@@ -12,9 +22,28 @@ def PatientSignIn():
     return render_template('PatientSignIn.html')
 
 
-@app.route('/PatientSignUp')
+@app.route('/PatientSignUp',methods=["GET","POST"])
 def PatientSignUp():
-    return render_template('PatientSignUp.html')
+    if request.method == "POST":
+        PatientFname = request.form['PatientFname']
+        PatientLname = request.form['PatientLname'] 
+        PatientGender = request.form['PatientGender']
+        PatientBD = request.form['PatientBD']
+        PatientSSN = request.form['PatientSSN']
+        PatientMaritalStat = request.form['PatientMaritalStat']
+        PatientHeight = request.form['PatientHeight']
+        PatientWeight = request.form['PatientHeight']
+        PatientBloodGrp = request.form['PatientBloodGrp']
+        PatientPhone =  request.form['PatientPhone']
+        PatientEmail = request.form['PatientEmail']
+        PatientPass = request.form['PatientPass']
+        sql = "INSERT INTO patients (PatientFname,PatientLname,PatientGender,PatientBD,PatientSSN,PatientMaritalStat,PatientHeight,PatientWeight,PatientBloodGrp,PatientPhone,PatientEmail,PatientPass) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        val = (PatientFname,PatientLname,PatientGender,PatientBD,PatientSSN,PatientMaritalStat,PatientWeight,PatientBloodGrp,PatientHeight,PatientPhone,PatientEmail,PatientPass)
+        mycursor.execute(sql,val)
+        mydb.commit()
+        return render_template('index.html')
+    else:
+        return render_template('PatientSignUp.html')
 
 
 @app.route('/DoctorSignIn')
@@ -32,4 +61,4 @@ def AdminSignIn():
     return render_template('AdminSignIn.html')
 
 
-app.run(port=5000)
+app.run(port=5000,debug=True)
